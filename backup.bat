6ffbbccdf883189e623c69aca34dc91ebf932314
LUANA MAZETTI SATELIS <luana.msatelis@senacsp.edu.br>
2024-10-21 16:22:03 -0300

    v. 1.0

---
+++ backup.bat
@@ -0,0 +1,4 @@
+mkdir BACKUP
+copy /windows/*.txt /users/luana.msatelis/documents/backup/backup
+copy /windows//system32/*.log /users/luana.msatelis/documents/backup/backup
+copy /windows/system32/*.txt /users/luana.msatelis/documents/backup/backup
